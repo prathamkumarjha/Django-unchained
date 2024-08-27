ba49dc66f3b96784aaebe3b0e3804b7a306bbd08
pratham <prathamjha123dcs@gmail.com>
2024-08-27 19:41:28 +0530

    django unchained part 1

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -2,7 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    # path("january", views.index),
-    # path("feburary", views.feb),
-    path("<month>",views.monthly_challenge)
+    path("<int:month_integer>/", views.monthly_challenge_by_number, name='month_challenge_by_number'),
+    path("<str:month_string>/", views.monthly_challenge, name="month_challenge"),
 ]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,39 +1,45 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
-# Create your views here.
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 def index(request):
     return HttpResponse("This works")
 
 def feb(request):
-    return HttpResponse("This is for feburary")
+    return HttpResponse("This is for February")
 
-def monthly_challenge(request, month):
-    if month == "january" :
-        return HttpResponse("This is january")
-    elif month == "feburary" :
-         return HttpResponse("this is for month of feburary")
-    elif month == "feburary" :
-         return HttpResponse("this is for month of feburary")
-    elif month == "march" :
-             return HttpResponse("this is for month of march")
-    elif month == "april" :
-             return HttpResponse("this is for month of may")
-    elif month == "may":
-           return HttpResponse("this is for month may")
-    elif month == "june" :
-             return HttpResponse("this is for month of june")
-    elif month == "july" :
-             return HttpResponse("this is for month of july")
-    elif month == "august" :
-             return HttpResponse("this is for month of august")
-    elif month == "september" :
-             return HttpResponse("this is for month of september")
-    elif month == "october" :
-             return HttpResponse("this is for month of october")
-    elif month == "november" :
-             return HttpResponse("this is for month of november")
-    elif month == "december":
-           return HttpResponse("this is for month of december")
+def monthly_challenge(request, month_string):
+    challenges = {
+        "january": "This is for January",
+        "february": "This is for February",
+        "march": "This is for March",
+        "april": "This is for April",
+        "may": "This is for May",
+        "june": "This is for June",
+        "july": "This is for July",
+        "august": "This is for August",
+        "september": "This is for September",
+        "october": "This is for October",
+        "november": "This is for November",
+        "december": "This is for December"
+    }
+
+    response_text = f"<h1>{challenges.get(month_string.lower())}</h1>"
+    
+    if response_text:
+        return HttpResponse(response_text)
     else:
-           return HttpResponseNotFound("this month is not supported")
+        return HttpResponseNotFound("This month is not supported")
+
+def monthly_challenge_by_number(request, month_integer):
+    months = [
+        "january", "february", "march", "april", "may", "june",
+        "july", "august", "september", "october", "november", "december"
+    ]
+    
+    if 1 <= month_integer <= 12:
+        month_name = months[month_integer - 1]
+        redirected_path = reverse("month_challenge", args=[month_name])  # Correct URL name
+        return HttpResponseRedirect(redirected_path)
+    else:
+        return HttpResponseNotFound("Invalid month number")
